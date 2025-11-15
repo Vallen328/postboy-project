5084aff73e46b98411fcb5e09c376f80022f5df9
Vallen Dsouza <123362088+Vallen328@users.noreply.github.com>
2025-11-16 00:19:52 +0530

    03_authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { currentUser } from "@/modules/authentication/actions";
+import UserButton from "@/modules/authentication/components/user-button";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser()
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Hello</Button>
+      <UserButton user = {user} />
     </div>
   );
 }
